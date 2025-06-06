e94f05d702cbc846ef030bfbbf49d8e0d9ef85ac
LuisQuintana23 <luisquintana2326@gmail.com>
2025-06-05 18:39:24 -0600

    feat: add printf argument %d

--- examples/program.c
+++ examples/program.c
@@ -1,6 +1,8 @@
 int main() {
     int x;
-    x = 10;
-    printf("It works again yeah! \n");
+    int y;
+    x = 11;
+    y = x + 2;
+    printf("It works again yeah! %d \n", y);
     return 0;
 }
